044a4452bde8a698f1de22a87e34fc0e4bdc12b0
realkendo <kennethistifanus1@gmail.com>
2024-11-06 13:33:29 +0100

    finalizing ui

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 window.onload = () =>{
-  console.log("loaded");
+  let form = document.getElementById("#myForm"),
+  imgInput = document.querySelector(".img"),
+  file = document.getElementById("imgInput"),
+  name = document.getElementById("name"),
+  age = document.getElementById("age"),
+  city = document.getElementById("city"),
+  email = document.getElementById("email"),
+  phone = document.getElementById("phone"),
+  post = document.getElementById("post"),
+  sDate = document.getElementById("sDate"),
+  submitBtn = document.querySelector(".submit"),
+  userInfo = document.getElementById("data");
+
+  let getDate = new Date();
 }
 
 
